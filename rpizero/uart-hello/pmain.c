1f5a6826aab38eafea687ca05f61de93b099391b
Petru Marginean <petru.marginean@gmail.com>
2017-10-08 10:02:19 -0400

    misc

--- rpizero/uart-hello/pmain.c
+++ rpizero/uart-hello/pmain.c
@@ -100,7 +100,7 @@ void uart_init()
 void uart_putc(unsigned char c)
 {
     // Wait for UART to become ready to transmit.
-    while ( mmio_read(UART0_FR) & (1 << 5) )
+    while (mmio_read(UART0_FR) & (1 << 5))
     {
     }
     mmio_write(UART0_DR, c);
@@ -109,7 +109,7 @@ void uart_putc(unsigned char c)
 unsigned char uart_getc()
 {
     // Wait for UART to have received something.
-    while ( mmio_read(UART0_FR) & (1 << 4) )
+    while (mmio_read(UART0_FR) & (1 << 4))
     {
     }
     return mmio_read(UART0_DR);
