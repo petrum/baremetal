75f4412d60312c2df0ccdef9b1eaba1be0568804
Petru Marginean <petru.marginean@gmail.com>
2017-10-08 08:32:32 -0400

    misc

--- rpizero/uart-hello/build.sh
+++ rpizero/uart-hello/build.sh
@@ -3,4 +3,5 @@
 arm-none-eabi-gcc -Wall -O2 -nostdlib -nostartfiles -ffreestanding  -c pmain.c
 arm-none-eabi-as start.asm -o start.o
 arm-none-eabi-ld start.o pmain.o -o uart-hello.elf
+arm-none-eabi-objcopy uart-hello.elf -O binary uart-hello.bin
 
