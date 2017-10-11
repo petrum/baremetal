dd9a74aed086494aaab61ffc17f29f0b9fd24c23
Petru Marginean <petru.marginean@gmail.com>
2017-10-10 20:25:12 -0400

    misc

--- rpizero/uart-hello/pmain.c
+++ rpizero/uart-hello/pmain.c
@@ -1,6 +1,14 @@
+#include <stdint.h>
+
 extern void PUT32 ( unsigned int, unsigned int );
 extern unsigned int GET32 ( unsigned int );
 extern void dummy ( unsigned int );
+
+static inline void delay(int32_t count)
+{
+    asm volatile("__delay_%=: subs %[count], %[count], #1; bne __delay_%=\n"
+                 : : [count]"r"(count) : "cc");
+}
 
 #define GPFSEL1 0x20200004
 #define GPPUD       0x20200094
@@ -53,46 +61,52 @@ int notmain ( unsigned int earlypc )
 {
     unsigned int ra;
 
-    PUT32(AUX_ENABLES,1);
-    PUT32(AUX_MU_IER_REG,0);
-    PUT32(AUX_MU_CNTL_REG,0);
-    PUT32(AUX_MU_LCR_REG,3);
-    PUT32(AUX_MU_MCR_REG,0);
-    PUT32(AUX_MU_IER_REG,0);
-    PUT32(AUX_MU_IIR_REG,0xC6);
-    PUT32(AUX_MU_BAUD_REG,270);
+    PUT32(AUX_ENABLES, 1);
+    PUT32(AUX_MU_IER_REG, 0);
+    PUT32(AUX_MU_CNTL_REG, 0);
+    PUT32(AUX_MU_LCR_REG, 3);
+    PUT32(AUX_MU_MCR_REG, 0);
+    PUT32(AUX_MU_IER_REG, 0);
+    PUT32(AUX_MU_IIR_REG, 0xC6);
+    PUT32(AUX_MU_BAUD_REG, 270);
 
-    ra=GET32(GPFSEL1);
-    ra&=~(7<<12); //gpio14
-    ra|=2<<12;    //alt5
-    ra&=~(7<<15); //gpio15
-    ra|=2<<15;    //alt5
-    PUT32(GPFSEL1,ra);
+    ra = GET32(GPFSEL1);
+    ra &= ~(7 << 12); //gpio14
+    ra |= 2 << 12;    //alt5
+    ra &= ~(7 << 15); //gpio15
+    ra |= 2 << 15;    //alt5
+    PUT32(GPFSEL1, ra);
    
     PUT32(GPPUD,0);
-    for(ra=0;ra<150;ra++) dummy(ra);
-    PUT32(GPPUDCLK0,(1<<14)|(1<<15));
-    for(ra=0;ra<150;ra++) dummy(ra);
-    PUT32(GPPUDCLK0,0);
+    for(ra = 0; ra < 150; ra++)
+        dummy(ra);
+    //delay(150);
+    PUT32(GPPUDCLK0, (1 << 14) | (1 << 15));
+    for(ra = 0; ra < 150; ra++)
+        dummy(ra);
+    //delay(150);
+    PUT32(GPPUDCLK0, 0);
     
-    PUT32(AUX_MU_CNTL_REG,3);
+    PUT32(AUX_MU_CNTL_REG, 3);
 
     hexstring(0x12345678);
     hexstring(earlypc);
 
-    while(1)
+    while (1)
     {
-        while(1)
+        while (1)
         {
-            if(GET32(AUX_MU_LSR_REG)&0x01) break;
+            if (GET32(AUX_MU_LSR_REG) & 0x01)
+                break;
         }
-        ra=GET32(AUX_MU_IO_REG);
-        //while(1)
-        //{
-            //if(GET32(AUX_MU_LSR_REG)&0x20) break;
-        //}
+        ra = GET32(AUX_MU_IO_REG);
+        while (1)
+        {
+            if (GET32(AUX_MU_LSR_REG) & 0x20)
+                break;
+        }
         PUT32(AUX_MU_IO_REG,ra);
     }
 
-    return(0);
+    return 0;
 }
