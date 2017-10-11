cde8c9d96504b921ac3463e3abdfa7fa7fee49a3
Petru Marginean <petru.marginean@gmail.com>
2017-10-10 20:00:16 -0400

    misc

--- rpizero/uart-hello/build.sh
+++ rpizero/uart-hello/build.sh
@@ -2,9 +2,10 @@
 set -e
 ./clean.sh
 
-arm-none-eabi-gcc -std=gnu99 -Wall -Wextra -O2 -nostdlib -nostartfiles -ffreestanding  -c pmain.c
+arm-none-eabi-gcc -Wall -Wextra -O2 -nostdlib -nostartfiles -ffreestanding  -c pmain.c
 arm-none-eabi-as -c start.asm -o start.o
 
 # the order of *.o is important
-arm-none-eabi-ld -T boot.ld -o uart-hello.elf -O2 -nostdlib start.o pmain.o
+arm-none-eabi-ld -T boot.ld -o uart-hello.elf start.o pmain.o
 arm-none-eabi-objcopy uart-hello.elf -O binary uart-hello.bin
+
