8d8936f0bafeedee47e01da9c8dcf70d4fffcdf0
Petru Marginean <petru.marginean@gmail.com>
2017-10-14 09:16:56 -0400

    misc

--- rpizero/led/led.c
+++ rpizero/led/led.c
@@ -1,49 +1,26 @@
-/* The base address of the GPIO peripheral (ARM Physical Address) */
-#define GPIO_BASE       0x20200000UL
-
-#define LED_GPFSEL      GPIO_GPFSEL1
-#define LED_GPFBIT      18
-#define LED_GPSET       GPIO_GPSET0
-#define LED_GPCLR       GPIO_GPCLR0
-#define LED_GPIO_BIT    16
-
-#define GPIO_GPFSEL1    1
-#define GPIO_GPSET0     7
-
-#define GPIO_GPCLR0     10
-
-/** GPIO Register set */
 volatile unsigned int* gpio;
-
-/** Simple loop variable */
 volatile unsigned int tim;
 
-/** Main function - we'll never return from here */
 int main(void) __attribute__((naked));
 int main(void)
 {
-    /* Assign the address of the GPIO peripheral (Using ARM Physical Address) */
-    gpio = (unsigned int*)GPIO_BASE;
+    gpio = (unsigned int*)0x20200000;
 
     /* Write 1 to the GPIO16 init nibble in the Function Select 1 GPIO
        peripheral register to enable GPIO16 as an output */
-    gpio[LED_GPFSEL] |= (1 << LED_GPFBIT);
+    gpio[1] |= (1 << 18);
 
-    /* Never exit as there is no OS to exit to! */
-    while(1)
+    while (1)
     {
         for(tim = 0; tim < 500000; tim++)
             ;
 
-        /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off
-           for plus models )*/
-        gpio[LED_GPCLR] = (1 << LED_GPIO_BIT);
+        /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off for plus models )*/
+        gpio[10] = (1 << 15);
 
         for(tim = 0; tim < 500000; tim++)
             ;
-
-        /* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on
-           for plus models )*/
-        gpio[LED_GPSET] = (1 << LED_GPIO_BIT);
+        /* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on for plus models )*/
+        gpio[7] = (1 << 15);
     }
 }
