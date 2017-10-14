fd930b71ffb371dba14c48a6387c0c18d2b9697d
Petru Marginean <petru.marginean@gmail.com>
2017-10-14 09:45:16 -0400

    misc

--- rpizero/led/build.sh
+++ rpizero/led/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 set -e
-arm-none-eabi-gcc -O0 -mfpu=vfp -nostartfiles -g led.c -o led.elf
+arm-none-eabi-gcc -nostartfiles -g led.c -o led.elf
 arm-none-eabi-objcopy led.elf -O binary led.img
 
