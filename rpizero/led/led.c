30cf104794e1a27fea3cb1c722b77f11674a53cc
Petru Marginean <petru.marginean@gmail.com>
2017-10-14 09:32:34 -0400

    misc

--- rpizero/led/led.c
+++ rpizero/led/led.c
@@ -5,22 +5,23 @@ int main(void) __attribute__((naked));
 int main(void)
 {
     gpio = (unsigned int*)0x20200000;
-
-    /* Write 1 to the GPIO16 init nibble in the Function Select 1 GPIO
-       peripheral register to enable GPIO16 as an output */
-    gpio[1] |= (1 << 18);
+    unsigned int ra;
+    ra = gpio[4];
+    ra &= ~(7<<21);
+    ra |= 1<<21;
+    gpio[4] = ra;
 
     while (1)
     {
-        for(tim = 0; tim < 500000; tim++)
+        for(tim = 0; tim < 5000000; tim++)
             ;
 
         /* Set the LED GPIO pin low ( Turn OK LED on for original Pi, and off for plus models )*/
-        gpio[10] = (1 << 15);
+        gpio[8] = (1 << 15);
 
-        for(tim = 0; tim < 500000; tim++)
+        for(tim = 0; tim < 5000000; tim++)
             ;
         /* Set the LED GPIO pin high ( Turn OK LED off for original Pi, and on for plus models )*/
-        gpio[7] = (1 << 15);
+        gpio[11] = (1 << 15);
     }
 }
---
+++ rpizero/led/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./build.sh
+load2rpi.sh led.img
+
