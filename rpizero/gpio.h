c7a7d225cf428c88a529079ad2d23827b4843f84
Petru Marginean <petru.marginean@gmail.com>
2017-10-15 11:04:23 -0400

    misc

---
+++ rpizero/gpio.h
@@ -0,0 +1,50 @@
+#ifndef __GPIO_H__
+#define __GPIO_H__
+
+struct GPIO
+{
+    enum Mode {In, Out, ALT0, ALT1, ALT2, ALT3, ALT4, ALT5};
+    static void init(int address);
+    static Mode getMode(int i);
+    static Mode setMode(int i, Mode m);
+    static void on(int i);
+    static void off(int i);
+private:
+    static volatile unsigned int* gpio_;
+};
+
+inline void GPIO::init(int a)
+{
+    gpio_ = (unsigned int*)a;
+}
+
+inline GPIO::Mode GPIO::getMode(int i)
+{
+}
+
+inline GPIO::Mode GPIO::setMode(int i, Mode m)
+{
+    //https://raspberrypi.stackexchange.com/questions/39252/on-board-led-connection-to-gpio
+    //the power LED (red) is connected to GPIO 35 (not present on the Pi Zero)
+    //the activity LED (green) is connected to GPIO 47
+    
+    //http://www.susa.net/wordpress/2012/07/raspberry-pi-gpfsel-gpio-and-pads-status-viewer/
+    //FSEL47 (GPFSEL4[21-23]) = 0 (GPIO In - [Low])
+    //In == 0, Out == 1, ALT0-ALT5 from 2 to 7 (total of 8, of three available bits)
+    gpio_[4] = (gpio_[4] & ~(7 << 21)) | 1 << 21;
+    //FSEL35 (GPFSEL3[15-17]) = 0 (GPIO In - [Low]): 
+}
+
+void GPIO::on(int i)
+{
+    gpio_[11] = 1 << (47 - 32);
+}
+
+void GPIO::off(int i)
+{
+    // the activity LED (green) is connected to GPIO 47
+    gpio_[8] = 1 << (47 - 32);
+}
+
+#endif
+
--- rpizero/led/led.cpp
+++ rpizero/led/led.cpp
@@ -1,3 +1,5 @@
+#include "../gpio.h"
+
 void delay(int i)
 {
     for(volatile int tim = 0; tim < i; tim++)
@@ -15,34 +17,33 @@ void longDelay()
         shortDelay();
 }
 
-volatile unsigned int* gpio;
-
-void off()
-{
-    // the activity LED (green) is connected to GPIO 47
-    gpio[8] = 1 << (47 - 32);
-}
-
-void on()
-{
-    gpio[11] = 1 << (47 - 32);
-}
-
 void dot()
 {
-    on(); shortDelay(); off(); shortDelay(); 
+    GPIO::on(47);
+    shortDelay();
+    GPIO::off(47);
+    shortDelay(); 
 }
 
 void line()
 {
-    on(); longDelay(); off(); shortDelay(); 
+    GPIO::on(47);
+    longDelay();
+    GPIO::off(47);
+    shortDelay(); 
 }
 
 void sos()
 {
-    dot(); dot(); dot();
-    line(); line(); line();
-    dot(); dot(); dot();
+    dot();
+    dot();
+    dot();
+    line();
+    line();
+    line();
+    dot();
+    dot();
+    dot();
 }
 
 //https://www.raspberrypi.org/forums/viewtopic.php?f=72&t=98904#p689471
@@ -62,6 +63,7 @@ void enableL1Cache()
     asm volatile ("mcr p15, 0, %0, c1, c0,  0" : : "r" (nControl) : "memory");    
 }
 
+volatile unsigned int* GPIO::gpio_;
 //https://www.evilsocket.net/2015/05/02/using-inline-assembly-and-naked-functions-to-fool-disassemblers/
 int main(void) __attribute__((naked)); // w/o this it doesn't work when booted directly (no u-boot)
 int main(void)
@@ -69,17 +71,8 @@ int main(void)
     enableBranchPrediction();
     enableL1Cache();
 
-    gpio = (unsigned int*)0x20200000;
-
-    //https://raspberrypi.stackexchange.com/questions/39252/on-board-led-connection-to-gpio
-    //the power LED (red) is connected to GPIO 35 (not present on the Pi Zero)
-    //the activity LED (green) is connected to GPIO 47
-    
-    //http://www.susa.net/wordpress/2012/07/raspberry-pi-gpfsel-gpio-and-pads-status-viewer/
-    //FSEL47 (GPFSEL4[21-23]) = 0 (GPIO In - [Low])
-    //In == 0, Out == 1, ALT0-ALT5 from 2 to 7 (total of 8, of three available bits)
-    gpio[4] = (gpio[4] & ~(7 << 21)) | 1 << 21;
-    //FSEL35 (GPFSEL3[15-17]) = 0 (GPIO In - [Low]): 
+    GPIO::init(0x20200000);
+    GPIO::setMode(47, GPIO::Out);
     while (true)
     {
         sos();
