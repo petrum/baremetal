795a822a16411a93d158f369a96161474791470b
Petru Marginean <petru.marginean@gmail.com>
2017-10-15 09:23:57 -0400

    misc

--- rpizero/led/build.sh
+++ rpizero/led/build.sh
@@ -3,7 +3,7 @@
 set -e
 rm -rf *.elf *.img
 # removing -nostartfiles ==> linking fails b/c it needs _exit: "undefined reference to `_exit'"
-arm-none-eabi-g++ -O3 -nostartfiles -std=c++11 led.cpp -o led.elf
+arm-none-eabi-g++ -O3 -nostartfiles -std=c++11 --entry=0x8000 led.cpp -o led.elf
 arm-none-eabi-nm led.elf
 arm-none-eabi-objcopy led.elf -O binary led.img
 
