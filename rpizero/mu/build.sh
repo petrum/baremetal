dadf56ce7863e930214d9d26282d1ba739994f75
Petru Marginean <petru.marginean@gmail.com>
2017-10-22 11:52:15 -0400

    misc

--- rpizero/mu/build.sh
+++ rpizero/mu/build.sh
@@ -3,7 +3,8 @@
 set -e
 ./clean.sh
 
-arm-none-eabi-g++ -O1 -nostdlib -nostartfiles -std=c++11 --entry=0x8000 main.cpp -o mu.elf
+# on the real microSD it works with '-O3' but not with '-O1'!
+arm-none-eabi-g++ -O3 -nostdlib -nostartfiles -std=c++11 --entry=0x8000 main.cpp -o mu.elf
 #arm-none-eabi-g++ -nostdlib -std=c++11 --entry=0x8000 main.cpp -o mu.elf
 arm-none-eabi-nm mu.elf | c++filt
 arm-none-eabi-objcopy mu.elf -O binary mu.img
