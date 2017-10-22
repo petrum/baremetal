0568065f3f62f026d9ca116d95d7f88ab08b059f
Petru Marginean <petru.marginean@gmail.com>
2017-10-21 20:50:26 -0400

    misc

--- rpizero/mu/build.sh
+++ rpizero/mu/build.sh
@@ -3,7 +3,7 @@
 set -e
 ./clean.sh
 
-arm-none-eabi-g++ -nostdlib -nostartfiles -std=c++11 --entry=0x8000 main.cpp -o mu.elf
+arm-none-eabi-g++ -nostdlib -std=c++11 --entry=0x8000 main.cpp -o mu.elf
 arm-none-eabi-nm mu.elf | c++filt
 arm-none-eabi-objcopy mu.elf -O binary mu.img
 size mu.elf | tee mu.size
