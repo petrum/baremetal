683bbc01df7926ee0d7c5819bee48d0c8709e568
Petru Marginean <petru.marginean@gmail.com>
2017-10-21 20:59:48 -0400

    misc

--- rpizero/led/build.sh
+++ rpizero/led/build.sh
@@ -7,4 +7,3 @@ arm-none-eabi-g++ -nostdlib -std=c++11 --entry=0x8000 main.cpp -o led.elf
 arm-none-eabi-nm led.elf | c++filt
 arm-none-eabi-objcopy led.elf -O binary led.img
 size led.elf | tee led.size
-
