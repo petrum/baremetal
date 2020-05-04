23cf46b5c5cf5d479613b15fa020cb7577ce6f13
Petru Marginean <petru.marginean@gmail.com>
2020-05-04 10:59:28 -0400

    misc

--- rpizero/rgb/build.sh
+++ rpizero/rgb/build.sh
@@ -4,6 +4,6 @@ set -e
 ./clean.sh
 # removing -nostartfiles ==> linking fails b/c it needs _exit: "undefined reference to `_exit'"
 arm-none-eabi-g++ -O3 -nostdlib -nostartfiles -std=c++11 --entry=0x8000 main.cpp -o rgb.elf
-arm-none-eabi-nm rgb.elf | c++filt
+#arm-none-eabi-nm rgb.elf | c++filt
 arm-none-eabi-objcopy rgb.elf -O binary rgb.img
-#size rgb.elf | tee rgb.size
+size rgb.elf | tee rgb.size
